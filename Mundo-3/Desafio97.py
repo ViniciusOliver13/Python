a6c847b1098e47a69897fb35bd3b86514f05a83c
Antonio Vinícius <146228058+ViniciusOliver13@users.noreply.github.com>
2024-01-16 15:18:53 -0300

    Add files via upload

---
+++ Mundo-3/Desafio97.py
@@ -0,0 +1,9 @@
+def escreva(palavra):
+    print('~' * (len(palavra) + 4 ), f'\n  {palavra}')
+    print('~' * (len(palavra) + 4))
+
+
+print('==== DESAFIO 97 ====')
+escreva('Olá, Mundo!')
+escreva('VINÍCIUS13')
+escreva('CURSO EM VÍDEO')
